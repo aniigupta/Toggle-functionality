98360b1d34f779b20c665fbb00bade510f40048e
aniigupta <aniket8076206264@gmail.com>
2023-04-09 22:10:59 +0530

    add

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function bulb_on(){
 function bulb_off(){
     document.getElementById('bulb').src="bulb-off.png";
 }
+// var flag =0
+// let bulb = document.getElementById('bulb')
+// function bulb_on(){
+//     // if(flag =0){
+//         bulb.classList.add("red")
+//     // }
+// }
+// // function bulb_off(){
+// //     document.getElementById('bulb').src="bulb-off.png";
+// // }
+
+// // funtion
